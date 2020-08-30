d6e836ccefd68b3e5b36f3f5495a78c4c9445a4b
mirsella <mirsella@protonmail.com>
2020-08-30 22:45:59 +0200

    gacp Sun Aug 30 10:45:59 PM CEST 2020

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 konsole -e bash -c "
-for file in documents/*/*; do mv -v "$file" "$(echo $file | sed 's/é/e/g')"; done
+for file in documents/*/*; do mv -v "$file" "$(sed 's/é/e/g; s/è/e/g' <<< $file)" 2> /dev/null; done
 git pull
 git add -A
 git commit -m \"upload.sh $(date)\"
