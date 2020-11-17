d8bd5a7e2a534271c7f601a5a079722054414f84
mirsella <mirsella@protonmail.com>
2020-11-18 00:04:51 +0100

    upload.sh Wed Nov 18 12:04:51 AM CET 2020

--- upload.sh
+++ upload.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-konsole -e bash -c "
-for file in documents/*/*; do mv -v "$file" "$(sed 's/é/e/g; s/è/e/g' <<< $file)" 2> /dev/null; done
-git pull --no-edit
+konsole -e bash -c '
+while read file; do
+  mv -v "${file}" "$(sed "s/é/e/g; s/è/e/g; s/ \?Microsoft Word//g" <<< ${file})"
+done <<< $(fd -d t "é|è|Microsoft Word")
+git pull --no-edit origin main
 git add -A
-git commit -m \"upload.sh $(date)\"
+git commit -m "upload.sh $(date)"
 git push origin main
-echo -e \"\nappuie sur une touche pour quitter\"
+echo -e "\nappuie sur une touche pour quitter"
 read
-"
+'
