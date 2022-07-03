51b2b93426289359c4e1ca8678ae029d4221d973
lacgt-cosmeva <cgtcosmeva@gmail.com>
2022-07-03 19:38:55 +0200

    upload.sh dim. 03 juil. 2022 19:38:55 CEST

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 konsole -e bash -c '
 while read file; do
   mv -v "${file}" "$(sed "s/é/e/g; s/è/e/g; s/ \?Microsoft Word//g" <<< ${file})"
-done <<< $(fd -d t "é|è|Microsoft Word")
+done <<< $(fd -t f "é|è|Microsoft Word")
 git pull --no-edit origin main
 git add -A
 git commit -m "upload.sh $(date)"
